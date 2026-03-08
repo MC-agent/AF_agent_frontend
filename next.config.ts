00873b5b93987c41262dbd9e001166440c17e7bb
byungjuchae <63639887+byoungjuchae@users.noreply.github.com>
2026-03-08 14:30:34 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
-  webpack: (config: Configuration) => {
-    config.watchOptions = {
-      poll: 3000,
-      aggregateTimeout: 500,
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
