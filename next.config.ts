c899746b914ddbee69731ffbc50187dcbbaf4b47
byungjuchae <63639887+byoungjuchae@users.noreply.github.com>
2026-03-08 14:28:15 +0900

    Refactor next.config.ts to use TypeScript types

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: () => {
-    
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration) => {
+    config.watchOptions = {
+      poll: 3000,
+      aggregateTimeout: 500,
+    };
+    return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
